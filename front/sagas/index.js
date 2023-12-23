f776c8bcc43675316333a0681dd18f866bdea99f
leejeawon92 <jeawon33333@gmail.com>
2023-12-23 17:59:55 +0900

    saga Separate

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,77 +1,10 @@
-import {all, call, fork, put, take, takeEvery, takeLatest} from 'redux-saga/effects';
-
-function logInAPI(data){
-  return axios.post('/api/login', data); // 실제 서버에 요청을 보낸다.
-}
-
-function* logIn(action){
-  try {
-    const result = yield call(logInAPI, action.data) // 서버로 로그인에 관련된 요청(logInAPI)을 보내는 API를 따로 빼고 그에 따른 결과값을 전달받는다. 
-    yield put({                         // logInAPI의 결과값이 실패냐 성공이냐에 따라 결과값이 달라진다.
-      type: 'LOG_IN_SUCCESS',
-      data: result.data
-    }) 
-  } catch (err) {
-    yield put({
-      type: 'LOG_IN_FAILURE',
-      data: err.response.data
-    }) 
-  }
-}
-
-function logOutAPI(){
-  return axios.post('/api/logout'); 
-}
-
-function* logOut(){
-  try {
-    const result = yield call(logOutAPI) 
-    yield put({                         
-      type: 'LOG_OUT_SUCCESS',
-      data: result.data
-    }) 
-  } catch (err) {
-    yield put({
-      type: 'LOG_OUT_FAILURE',
-      data: err.response.data
-    }) 
-  }
-}
-
-function addPostAPI(data){
-  return axios.post('/api/post',data); 
-}
-
-function* addPost(action){
-  try {
-    const result = yield call(addPostAPI,action.data) 
-    yield put({                         
-      type: 'ADD_POST_SUCCESS',
-      data: result.data
-    }) 
-  } catch (err) {
-    yield put({
-      type: 'ADD_POST_FAILURE',
-      data: err.response.data
-    }) 
-  }
-}
-function* watchLogin(){
-  yield takeLatest('LOG_IN_REQUEST', logIn)  
-}
-
-function* watchLogOut(){
-  yield takeLatest('LOG_OUT_REQUEST', logOut)
-}
-
-function* watchAddPost(){
-  yield takeLatest('ADD_POST_REQUEST',addPost)
-}
+import {all, fork } from 'redux-saga/effects';
+import postSaga from './post';
+import userSaga from './user';
 
 export default function* rootSaga() {
     yield all ([
-      fork(watchLogin),
-      fork(watchLogOut),
-      fork(watchAddPost)
+      fork(postSaga),
+      fork(userSaga),
     ])
 }
---
+++ front/sagas/user.js
@@ -0,0 +1,52 @@
+import { all, call, delay, fork, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+function logInAPI(data){
+  return axios.post('/api/login', data); // 실제 서버에 요청을 보낸다.
+}
+function logOutAPI(){
+  return axios.post('/api/logout'); 
+}
+
+function* logIn(action){
+  try {
+    // const result = yield call(logInAPI, action.data) // 서버로 로그인에 관련된 요청(logInAPI)을 보내는 API를 따로 빼고 그에 따른 결과값을 전달받는다. 
+    yield delay(1000)
+    yield put({                         // logInAPI의 결과값이 실패냐 성공이냐에 따라 결과값이 달라진다.
+      type: 'LOG_IN_SUCCESS',
+      data: result.data
+    }) 
+  } catch (err) {
+    yield put({
+      type: 'LOG_IN_FAILURE',
+      data: err.response.data
+    }) 
+  }
+}
+
+function* logOut(){
+  try {
+    yield delay(1000)
+    yield put({                         
+      type: 'LOG_OUT_SUCCESS',
+    }) 
+  } catch (err) {
+    yield put({
+      type: 'LOG_OUT_FAILURE',
+      data: err.response.data
+    }) 
+  }
+}
+function* watchLogIn(){
+  yield takeLatest('LOG_IN_REQUEST', logIn)  
+}
+
+function* watchLogOut(){
+  yield takeLatest('LOG_OUT_REQUEST', logOut)
+}
+export default function* userSaga(){
+  yield all([
+    fork(watchLogIn),
+    fork(watchLogOut)
+  ])
+}
