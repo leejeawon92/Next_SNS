06fec1a96bc6635770c109afc7be720e76cd25fc
leejeawon92 <jeawon33333@gmail.com>
2023-12-25 18:00:23 +0900

    eslint strong regulations

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,6 +1,6 @@
 import { all, call, fork, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
-import { ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_POST_FAILURE, ADD_POST_REQUEST, ADD_POST_SUCCESS, addComment } from '../reducers/post';
+import { ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_POST_FAILURE, ADD_POST_REQUEST, ADD_POST_SUCCESS } from '../reducers/post';
 
 function addPostAPI(data){
   return axios.post('/api/post',data); 
