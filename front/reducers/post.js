4fd53c503c2c70c752889f13b3a3a72a5e9b235e
leejeawon92 <jeawon33333@gmail.com>
2023-12-30 18:30:31 +0900

    Get data at last scrolling

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -14,41 +14,17 @@ export const REMOVE_POST_REQUEST = 'REMOVE_POST_REQUEST';
 export const REMOVE_POST_SUCCESS = 'REMOVE_POST_SUCCESS';
 export const REMOVE_POST_FAILURE = 'REMOVE_POST_FAILURE';
 
+export const LOAD_POSTS_REQUEST = 'LOAD_POSTS_REQUEST';
+export const LOAD_POSTS_SUCCESS = 'LOAD_POSTS_SUCCESS';
+export const LOAD_POSTS_FAILURE = 'LOAD_POSTS_FAILURE';
 
 export const initialState = {
-  mainPosts: [{
-    id: 1,
-    User: {
-      id: 1,
-      nickname: 'jeawon',
-    },
-    content: '첫 번째 게시글 #first #첫번째 # 첫번째',
-    Images: [{
-      id: shortid.generate(),
-      src: 'https://images.unsplash.com/photo-1581196607303-95c00f31c676?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fCVFQSVCMiVBOCVFQyU5QSVCOHxlbnwwfHwwfHx8MA%3D%3D',
-    }, {
-      id: shortid.generate(),
-      src: 'https://images.unsplash.com/photo-1445543949571-ffc3e0e2f55e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8JUVBJUIyJUE4JUVDJTlBJUI4fGVufDB8fDB8fHww',
-    }, {
-      id: shortid.generate(),
-      src: 'https://images.unsplash.com/photo-1457269449834-928af64c684d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVBJUIyJUE4JUVDJTlBJUI4fGVufDB8fDB8fHww',
-    }],
-    Comments: [{
-      id: shortid.generate(),
-      User: {
-        nickname: 'dumy1',
-      },
-      content: '더미1',
-    }, {
-      id: shortid.generate(),
-      User: {
-        id: shortid.generate(),
-        nickname: 'dumy2',
-      },
-      content: '더미2',
-    }]
-  }],
+  mainPosts: [],
   imagePaths: [],
+  hasMorePosts: true,
+  loadPostsLoading: false,
+  loadPostsDone: false,
+  loadPostsError: null,
   addPostLoading: false,
   addPostDone: false,
   addPostError: null,
@@ -80,26 +56,24 @@ const dummyComment = (data) => ({
   },
 });
 
-initialState.mainPosts = initialState.mainPosts.concat(
-  Array(20).fill().map(()=>({
+export const generateDummyPost = (number) => Array(number).fill().map(() => ({
+  id: shortid.generate(),
+  User: {
     id: shortid.generate(),
+    nickname: faker.internet.userName(),
+  },
+  content: faker.lorem.paragraph(),
+  Images: [{
+    src: faker.image.url(),
+  }],
+  Comments: [{
     User: {
       id: shortid.generate(),
       nickname: faker.internet.userName(),
     },
-    content: faker.lorem.paragraph(),
-    Images: [{
-      src: faker.image.url(),
-    }],
-    Comments: [{
-      User: {
-        id: shortid.generate(),
-        nickname: faker.internet.userName(),
-      },
-      content: faker.lorem.sentence(),
-    }],
-  }))
-)
+    content: faker.lorem.sentence(),
+  }],
+}));
 
 
 export const addPost = (data) => ({
@@ -115,6 +89,23 @@ export const addComment = (data) => ({
 export default (state = initialState, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
+      case LOAD_POSTS_REQUEST:
+        draft.loadPostsLoading = true;
+        draft.loadPostsDone = false;
+        draft.loadPostsError = null;
+        break;
+      case LOAD_POSTS_SUCCESS:
+        draft.loadPostsLoading = false;
+        draft.loadPostsDone = true;
+        draft.mainPosts = action.data.concat(draft.mainPosts);
+        draft.hasMorePosts = draft.mainPosts.length < 50;
+        break;
+      case LOAD_POSTS_FAILURE:
+        draft.loadPostsLoading = false;
+        draft.loadPostsError = action.error;
+        break;
+
+
       case ADD_POST_REQUEST:
         draft.addPostLoading = true;
         draft.addPostDone = false;
--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,8 +1,30 @@
-import { all, delay, fork, put, takeLatest } from 'redux-saga/effects';
-import { ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_POST_FAILURE, ADD_POST_REQUEST, ADD_POST_SUCCESS, REMOVE_POST_FAILURE, REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS } from '../reducers/post';
+import { all, delay, fork, put, takeLatest, throttle } from 'redux-saga/effects';
+import { ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_POST_FAILURE, ADD_POST_REQUEST, ADD_POST_SUCCESS, LOAD_POSTS_FAILURE, LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, REMOVE_POST_FAILURE, REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, generateDummyPost } from '../reducers/post';
 import axios from 'axios';
 import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from '../reducers/user';
 import shortid from 'shortid';
+
+
+function loadPostsAPI(data) {
+  return axios.get('/api/posts', data);
+}
+function* loadPosts(action) {
+  try {
+    // const result = yield call(loadPostsAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: LOAD_POSTS_SUCCESS,
+      data: generateDummyPost(10),
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: LOAD_POSTS_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
+
 
 function addPostAPI(data){
   return axios.post('/api/post',data); 
@@ -76,6 +98,11 @@ function* removePost(action) {
   }
 }
 
+
+function* watchLoadPosts() {
+  yield throttle(5000, LOAD_POSTS_REQUEST, loadPosts);
+}
+
 function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
 }
@@ -88,10 +115,12 @@ function* watchRemovePost(){
   yield takeLatest(REMOVE_POST_REQUEST,removePost);
 } 
 
+
 export default function* postSaga() {
     yield all ([
+      fork(watchLoadPosts),
       fork(watchAddPost),
       fork(watchAddComment),
-      fork(watchRemovePost)
+      fork(watchRemovePost),
     ])
 }
