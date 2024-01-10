7cd18c0c2b7213f72b8a84dde6b39125c69d47d4
leejeawon92 <jeawon33333@gmail.com>
2024-01-10 16:28:16 +0900

    Remain logged in after a refresh

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,8 +2,40 @@ const express = require('express');
 const {User, Post} = require('../models');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => { // GET /user
+  try {
+    if (req.user) {
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: req.user.id },
+        attributes: {
+          exclude: ['password']
+        },
+        include: [{
+          model: Post,
+          attributes: ['id'],
+        }, {
+          model: User,
+          as: 'Followings',
+          attributes: ['id'],
+        }, {
+          model: User,
+          as: 'Followers',
+          attributes: ['id'],
+        }]
+      })
+      res.status(200).json(fullUserWithoutPassword);
+    } else {
+      res.status(200).json(null);
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 router.post('/login', isNotLoggedIn ,(req, res, next)=>{ // POST /user/login
   passport.authenticate('local', (err, user, info)=>{  
@@ -26,12 +58,15 @@ router.post('/login', isNotLoggedIn ,(req, res, next)=>{ // POST /user/login
         },
         include: [{
           model: Post,
+          attributes: ['id'],
         }, {
           model: User,
           as: 'Followings',
+          attributes: ['id'],
         }, {
           model: User,
           as: 'Followers',
+          attributes: ['id'],
         }]
       })
       return res.status(200).json(fullUserWithoutPassword);
--- front/reducers/user.js
+++ front/reducers/user.js
@@ -3,23 +3,33 @@ import {produce} from 'immer';
 export const LOG_IN_REQUEST = 'LOG_IN_REQUEST'; 
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'; 
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'; 
+
 export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
 export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS'; 
 export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE'; 
+
 export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS'; 
 export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE'; 
+
 export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
 export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS'; 
 export const FOLLOW_FAILURE = 'FOLLOW_FAILURE'; 
+
 export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
 export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS'; 
 export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE'; 
+
 export const CHANGE_NICKNAME_REQUEST = 'CHANGE_NICKNAME_REQUEST';
 export const CHANGE_NICKNAME_SUCCESS = 'CHANGE_NICKNAME_SUCCESS'; 
 export const CHANGE_NICKNAME_FAILURE = 'CHANGE_NICKNAME_FAILURE'; 
+
 export const ADD_POST_TO_ME = 'ADD_POST_TO_ME';
 export const REMOVE_POST_OF_ME = 'REMOVE_POST_OF_ME';
+
+export const LOAD_MY_INFO_REQUEST = 'LOAD_MY_INFO_REQUEST';
+export const LOAD_MY_INFO_SUCCESS = 'LOAD_MY_INFO_SUCCESS';
+export const LOAD_MY_INFO_FAILURE = 'LOAD_MY_INFO_FAILURE';
 
 
 const dummyUser = (data) => ({
@@ -32,6 +42,9 @@ const dummyUser = (data) => ({
 });
 
 export const initialState = {
+  loadMyInfoLoading: false, // 유저 정보 가져오기 시도중
+  loadMyInfoDone: false,
+  loadMyInfoError: null,
   followLoading: false, // 팔로우 시도중
   followDone: false,
   followError: null,
@@ -163,6 +176,22 @@ export default (state = initialState, action) => {
       case CHANGE_NICKNAME_FAILURE:
         draft.changeNicknameLoading = false;
         draft.changeNicknameError = action.error;
+        break;
+
+
+      case LOAD_MY_INFO_REQUEST:
+        draft.loadMyInfoLoading = true;
+        draft.loadMyInfoError = null;
+        draft.loadMyInfoDone = false;
+        break;
+      case LOAD_MY_INFO_SUCCESS:
+        draft.loadMyInfoLoading = false;
+        draft.me = action.data;
+        draft.loadMyInfoDone = true;
+        break;
+      case LOAD_MY_INFO_FAILURE:
+        draft.loadMyInfoLoading = false;
+        draft.loadMyInfoError = action.error;
         break;
 
 
